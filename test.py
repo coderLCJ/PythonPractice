7976b282105091c82e4bb4c6da5d5cd91bef50e3
coderLCJ <2907805535@qq.com>
2021-01-20 21:47:46 +0800

    update

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-L = filter(lambda x: x % 2 == 1, list(range(1, 20)))
-print(list(L))
+' a test module '
+
+__author__ = 'Michael Liao'
+
+import sys
+
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello, world!')
+    elif len(args) == 2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+
+
+if __name__ == '__main__':
+    test()
