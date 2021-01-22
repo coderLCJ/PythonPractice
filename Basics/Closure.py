dcb1339b7dba99cd48ed878f0aea287fb69a3d28
coderLCJ <2907805535@qq.com>
2021-01-22 22:24:05 +0800

    Default Changelist

--- Basics/Closure.py
+++ Basics/Closure.py
@@ -12,6 +12,6 @@ counterA = createCounter()
 print(counterA(), counterA(), counterA(), counterA(), counterA())  # 1 2 3 4 5
 counterB = createCounter()
 if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
-    print('测试通过!')
+    print('测试通过')
 else:
-    print('测试失败!')
+    print('测试失败')
