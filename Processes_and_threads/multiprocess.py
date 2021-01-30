bc7b01308ddf10f7868791de30fd47c3a8d0e81b
coderLCJ <2907805535@qq.com>
2021-01-30 19:18:33 +0800

    Default Changelist

--- Processes_and_threads/multiprocess.py
+++ Processes_and_threads/multiprocess.py
@@ -1,2 +1,14 @@
 from multiprocessing import Process
 import os
+
+def run_child(name):
+    print('Child process \'%s\' is running (%s)' % (name, os.getpid()))
+
+
+if __name__ == '__main__':
+    print('Parent process \'%s\' is running (%s)' % ('main', os.getpid()))
+    p = Process(target=run_child, args=('test',))
+    print('Child process will start....')
+    p.start()       # 启动子进程
+    p.join()        # 等待子进程结束后再继续往下运行
+    print('END')
