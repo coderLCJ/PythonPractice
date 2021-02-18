1181735fbc4395df7afb6f1dd89c62e1886a7ab2
coderLCJ <2907805535@qq.com>
2021-02-18 22:54:33 +0800

    Default Changelist

--- webspider/DataStorage/save_as_csv.py
+++ webspider/DataStorage/save_as_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('data4.csv', 'w') as csvfile:
+with open('data4.csv', 'w', newline='') as csvfile:  # newline=''不会空行
     writer = csv.writer(csvfile)
     writer.writerow(['id', 'name', 'age'])
     writer.writerow(['10001', 'Bob', 20])
